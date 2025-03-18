a69d2eb170e015a94f75df849e46b982464a51e3
mastermind117 <trading.courses.2000@gmail.com>
2025-03-18 01:59:43 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
-  author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  packname: 'ODS TECHNOLOGIES',
+  author: 'ODS TECHNOLOGIES',
+  botName: "ODS TECHNOLOGIES",
+  botOwner: 'ODS TECHNOLOGIES', // Your name
+  ownerNumber: '233596780685', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
